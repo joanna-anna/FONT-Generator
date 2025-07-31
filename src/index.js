d31fc7c29d564bc0e7f587412cd0c3da59abe2eb
joanna-anna <jo.an.jr.promo@gmail.com>
2025-07-31 10:30:15 +0200

    Added basic form handling and added Typewriting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateFont(event) {
+  event.preventDefault();
 
+  new Typewriter("#font", {
+    strings: "Helvica is always the best",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let FontElement = document.querySelector("#font-generation");
+FontElement.addEventListener("submit", generateFont);
